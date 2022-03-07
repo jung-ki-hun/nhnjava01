425a81c3be35fc54f1eaf2bf9289749712f0e675
jungkihun <khkh0130@gmail.com>
2022-03-07 11:55:05 +0900

    jkh comman edit

--- src/nhn01/Ex01.java
+++ src/nhn01/Ex01.java
@@ -23,16 +23,23 @@ public class Ex01 {
 	}
 }
 
-/**
-객체 -> 틀
-클래스 -> 틀로 찍어낸 것
+/*
 
-class Hw{
-	...
-}// 객체
+///* 클래스 
+public class Animal {
+  ...
+}
+///* 객체와 인스턴스 
+public class Main {
+  public static void main(String[] args) {
+    Animal cat, dog; // '객체'
 
-Hw hw = new Hw();
-hw // 클래스
+    // 인스턴스화
+    cat = new Animal(); // cat은 Animal 클래스의 '인스턴스'(객체를 메모리에 할당)
+    dog = new Animal(); // dog은 Animal 클래스의 '인스턴스'(객체를 메모리에 할당)
+  }
+}
+https://gmlwjd9405.github.io/2018/09/17/class-object-instance.html
 
 인자 -> value 즉 값
 매개변ㅅ -> 변수 즉 메모리에 할당하는 공간
