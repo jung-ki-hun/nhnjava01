5fd8e0018344b858f328417b6f5e2bdba94ac5df
jungkihun <khkh0130@gmail.com>
2022-03-07 10:14:42 +0900

    jkh edit 01

--- src/nhn01/Ex01.java
+++ src/nhn01/Ex01.java
@@ -1,5 +1,19 @@
 package nhn01;
 
-public class Ex01 {
+import java.io.*;
 
+class HelloWorld{
+	public HelloWorld(String[] args) {
+		System.out.println("helloworld");
+		int size = args.length;
+		for(int i =0;i<size;i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
+public class Ex01 {
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld(args);
+		System.out.println(hw);
+	}
 }
