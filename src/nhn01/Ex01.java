7951af99f168addfee6105d3b40a09711a62356c
jungkihun <khkh0130@gmail.com>
2022-03-07 10:27:47 +0900

    jkh edit ex01

--- src/nhn01/Ex01.java
+++ src/nhn01/Ex01.java
@@ -10,10 +10,14 @@ class HelloWorld{
 			System.out.println(args[i]);
 		}
 	}
+	public int add(int a,int b){
+		return a+b;
+	}
 }
 public class Ex01 {
 	public static void main(String[] args) {
 		HelloWorld hw = new HelloWorld(args);
 		System.out.println(hw);
+		System.out.println(hw.add(2,3));
 	}
 }
